a3f81aa5d105183ed999fe43f58ef5c8bc09bdd1
Vivek Gudipudi <vivekgudipudi@gmail.com>
2023-08-22 15:35:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+
+const socket = io.connect("https://socketio-experiments-backend.vercel.app/");
+const userName = nanoid(4);
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(()=>{
+    socket.on("chat",(payload)=>setChat([...chat,payload]))
+  })
+
+  const sendChat = (e)=>{
+    e.preventDefault();
+    socket.emit("chat",{message,userName});
+    setMessage('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>socket io</h1>
+        {chat.map((payload,index)=>{
+          return(
+            <p key={index}>{payload.userName} : {payload.message}</p>
+          )
+        })}
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">send</button>
+        </form>
       </header>
     </div>
   );
